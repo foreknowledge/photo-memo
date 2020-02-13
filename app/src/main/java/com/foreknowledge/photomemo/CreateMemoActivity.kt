d2fe26c5e781999ebc4dd411c9c24144a6777309
s2choco <kyj_0129@naver.com>
2020-02-14 00:03:49 +0900

    샘플 Url로 이미지 가져오기 구현

--- app/src/main/java/com/foreknowledge/photomemo/CreateMemoActivity.kt
+++ app/src/main/java/com/foreknowledge/photomemo/CreateMemoActivity.kt
@@ -56,52 +56,65 @@ class CreateMemoActivity : AppCompatActivity() {
             .setTitle(resources.getString(R.string.text_add_image))
             .setItems(options){ _, i ->
                 when (i) {
-                    0 -> setGalleryImage()
-                    1 -> setPicturedImage()
-                    2 -> Toast.makeText(context, "이미지 url", Toast.LENGTH_SHORT).show()
+                    0 -> switchToAlbum()
+                    1 -> switchToCamera()
+                    2 -> setUrlImage()
                 }
             }.show()
     }
 
-    private fun setPicturedImage() {
-        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
-        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
-        file = File.createTempFile("JPEG_${timeStamp}_", ".jpg", storageDir)
-
-        val uri: Uri = FileProvider.getUriForFile(this, "com.foreknowledge.photomemo.fileprovider", file)
-
-        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, uri)
-
-        startActivityForResult(intent, CHOOSE_CAMERA_IMAGE)
-    }
-
-    private fun setGalleryImage() {
+    private fun switchToAlbum() {
         val intent = Intent(Intent.ACTION_PICK)
         intent.type = MediaStore.Images.Media.CONTENT_TYPE
 
         startActivityForResult(intent, CHOOSE_GALLERY_IMAGE)
     }
 
+    private fun switchToCamera() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, createUri())
+
+        startActivityForResult(intent, CHOOSE_CAMERA_IMAGE)
+    }
+
+    private fun createUri(): Uri {
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
+        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        file = File.createTempFile("JPEG_${timeStamp}_", ".jpg", storageDir)
+
+        return FileProvider.getUriForFile(this, "${packageName}.fileprovider", file)
+    }
+
+    private fun setUrlImage() {
+        val urlStr = "https://t1.daumcdn.net/thumb/R720x0/?fname=http://t1.daumcdn.net/brunch/service/user/5xq2/image/w2gbbJ7lwG0quKMZtTihufPuvno.jpg"
+
+        ImageLoadTask(urlStr, imagesAdapter).execute()
+    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        var imageBitmap: Bitmap? = null
-
         if (resultCode == Activity.RESULT_OK) {
-            if (requestCode == CHOOSE_GALLERY_IMAGE && data != null && data.data != null)
-                imageBitmap = tryReadBitmap(data.data!!)
-            else if (requestCode == CHOOSE_CAMERA_IMAGE)
-                imageBitmap = BitmapFactory.decodeFile(file.absolutePath)?.getRotateBitmap(file.absolutePath)
-        }
-
-        imageBitmap?.let {
-            if (imagesAdapter.addImage(imageBitmap))
-                imagesAdapter.notifyDataSetChanged()
-            else
-                Toast.makeText(context, "이미지 첨부는 ${MAX_IMAGE_COUNT}개까지만 가능합니다.", Toast.LENGTH_SHORT).show()
+            if (requestCode == CHOOSE_GALLERY_IMAGE && data != null && data.data != null) {
+                imagesAdapter.addBitmapImage(tryReadBitmap(data.data!!))
+            } else if (requestCode == CHOOSE_CAMERA_IMAGE) {
+                imagesAdapter.addBitmapImage(decodeFileAndRotate(file.absolutePath))
+            }
         }
     }
+
+    private fun ImageListAdapter.addBitmapImage(bitmap: Bitmap?) {
+        if (bitmap != null) {
+            if (this.addImage(bitmap)) this.notifyDataSetChanged()
+            else Toast.makeText(
+                context,
+                "이미지 첨부는 ${MAX_IMAGE_COUNT}개까지만 가능합니다.",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+    private fun decodeFileAndRotate(filePath: String) = BitmapFactory.decodeFile(filePath)?.getRotateBitmap(filePath)
 
     private fun tryReadBitmap(data: Uri): Bitmap? {
         val inputStream: InputStream? = contentResolver.openInputStream(data)
---
+++ app/src/main/java/com/foreknowledge/photomemo/ImageLoadTask.kt
@@ -0,0 +1,27 @@
+package com.foreknowledge.photomemo
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.AsyncTask
+import java.net.URL
+
+class ImageLoadTask(private val urlStr: String, private val adapter: ImageListAdapter) : AsyncTask<Void, Void, Bitmap>() {
+    companion object {
+        private val bitmapHash = mutableMapOf<String, Bitmap>()
+    }
+
+    override fun doInBackground(vararg p0: Void): Bitmap? =
+        if (bitmapHash.containsKey(urlStr)) bitmapHash[urlStr]
+        else {
+            val bitmap = BitmapFactory.decodeStream(URL(urlStr).openConnection().getInputStream())
+            bitmapHash[urlStr] = bitmap
+            bitmap
+        }
+
+    override fun onPostExecute(bitmap: Bitmap) {
+        super.onPostExecute(bitmap)
+        adapter.addImage(bitmap)
+        adapter.notifyDataSetChanged()
+    }
+
+}
