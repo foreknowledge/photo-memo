6585a9c507ca984372ab108e180ed8c67913c104
s2choco <kyj_0129@naver.com>
2020-02-22 16:14:32 +0900

    사용한 외부 라이브러리 주석으로 표기

--- app/src/main/java/com/foreknowledge/photomemo/SplashActivity.kt
+++ app/src/main/java/com/foreknowledge/photomemo/SplashActivity.kt
@@ -4,6 +4,12 @@ import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 
+/*
+[사용한 외부 라이브러리]
+'com.github.pedroSG94:AutoPermissions:1.0.3'
+'com.github.chrisbanes:PhotoView:2.1.3'
+*/
+
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
